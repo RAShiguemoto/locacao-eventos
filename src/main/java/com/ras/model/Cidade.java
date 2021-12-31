d12ce74cc7414960fd6f6497a1beeb02c8608c10
RAS <raulaguirreshiguemoto@gmail.com>
2021-12-31 19:15:16 -0300

    Inserindo mascaras de cep e telefone. Corrigindo bug de autocomplete cidade (Cadastro de clienete).

--- src/main/java/com/ras/model/Cidade.java
+++ src/main/java/com/ras/model/Cidade.java
@@ -23,4 +23,9 @@ public class Cidade implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    
+    @Override
+    public String toString() {
+        return id.toString();
+    }
 }
